4ab57d67be6fe23756265ec5b4f2df375944dd9a
boren <757967017@qq.com>
2017-04-05 14:57:44 +0800

    test

--- test.php
+++ test.php
@@ -5,7 +5,16 @@
  * Date: 17/4/5
  * Time: 下午2:22
  */
-require_once __DIR__ . '/mongodb.php';
+//require_once __DIR__ . '/mongodb.php';
 
-$client = mongodb::getInstance();
-$client->insert('test',array());
+//$client = mongodb::getInstance();
+//$client->insert('test',array());
+$m = new MongoClient('mongodb://192.168.1.17:30000/user?slaveOk=false');
+
+$db = $m->user;
+
+$coll = $db->xx;
+
+$document = array( "title" => "Calvin and Hobbes", "author" => "Bill Watterson" );
+
+$coll->insert($document);
