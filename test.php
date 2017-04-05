5c5687a117df8b9678e4a2dcf1fb16e4d1b68125
boren <757967017@qq.com>
2017-04-05 16:16:30 +0800

    test

--- test.php
+++ test.php
@@ -9,7 +9,7 @@
 
 //$client = mongodb::getInstance();
 //$client->insert('test',array());
-$manager = new MongoDB\Driver\Manager('mongodb://zuihuiyou_user:zuihuiyou_2014@192.168.1.17:30000');
+$manager = new MongoDB\Driver\Manager('mongodb://zuihuiyou_user:zuihuiyou_2014@192.168.1.17:30000/user');
 //$manager->authenticate("zuihuiyou_user","zuihuiyou_2014");
 $bulk = new MongoDB\Driver\BulkWrite;
 $bulk->insert(['x' => 1, 'name'=>'菜鸟教程', 'url' => 'http://www.runoob.com']);
